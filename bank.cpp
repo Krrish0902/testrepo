579d965e7126f0afdd1a31e9d0f28117d5b881e5
ananthakrishnan <ananthuk0902@gmail.com>
2023-10-28 22:47:35 +0530

    added some oeprations

--- bank.cpp
+++ bank.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 class bank
 {
-    private:int amt,bal,t,mb;
+    private:int amt,bal,t;
     public:void beq();
            void deposit();
            void withdrawal();
@@ -12,37 +12,76 @@ void bank::put()
 {
     cout<<"enter the type account type\n1.savings\n2.current";
     cin>>t;
-     switch(t)
+    if(t==1)
+        bal=1000;
+    else
+        bal=5000;
+}
+void bank::withdrawal()
+{
+    cout<<"enter the amout to be withdrawn";
+    cin>>amt;
+    if(amt>bal)
     {
-    case 1:
-        mb=1000;
-    case 2:
-        mb==5000;
+        cout<<"insuufficient balance";
+    }
+    else
+    {
+        bal=bal-amt;
+        cout<<bal;
     }
 }
-void bank::beq()
-{}
 void bank::deposit()
-{}
-void bank::withdrawal()
-{}
+{
+    cout<<"enter the amount to be deposited";
+    cin>>amt;
+    bal=bal+amt;
+    cout<<bal;
+}
+void bank::beq()
+{
+    cout<<bal;
+}
 int main()
 {
 
-    int an,o;
+    int an,R,o,R1;
     bank cus[100];
-        cout<<"enter your account number[0-99]";
-        cin>>an;
-        cus[an].put();
-        cout<<"setect the operation you need to do\n1.withdrawal\n2.deposit\n3.balance enquiry";
-        cin>>o;
-        switch(o)
+    label1:
         {
-        case 1:
-            cus[an].withdrawal();
-        case 2:
-            cus[an].deposit();
-        case 3:
-            cus[an].beq();
+    cout<<"enter your account number[0-99]";
+    cin>>an;
+    cus[an].put();
+    label2:
+        {
+    cout<<"setect the operation you need to do\n1.withdrawal\n2.deposit\n3.balance enquiry";
+    cin>>o;
+    if(o==1)
+        cus[an].withdrawal();
+    else if(o==2)
+        cus[an].deposit();
+    else
+        {
+        cus[an].beq();
+        cout<<"\naccount number:"<<an;
         }
+        }
+        }
+    cout<<"\ndo you wanna continue\n1.yess\n2.noo";
+    cin>>R;
+    if(R==1)
+    {
+        cout<<"do you want to continue in same account or another\n1.same\n2.another";
+        cin>>R1;
+        if(R1==2)
+        {
+            goto label1;
+        }
+        else
+            goto label2;
+    }
+    else
+    {
+        return 0;
+    }
 }
