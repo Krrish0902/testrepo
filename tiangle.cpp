f5e9cc3ac63cd1dbacb439187bda0a393bdd2170
ananthakrishnan <ananthuk0902@gmail.com>
2023-10-29 16:07:09 +0530

    first version of commit

---
+++ tiangle.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+class triangle
+{
+    private:int x,y,z,a=0;
+            float s,ar;
+    public:void valid();
+           void sides();
+           void area();
+           void assign();
+};
+void triangle::assign()
+{
+    cout<<"enter the sides of the triangle";
+    cin>>x>>y>>z;
+    s=(x+y+z)/2;
+}
+void triangle::valid()
+{
+    if((x>y+z)||(y>x+z)||(z>x+y))
+    {
+        cout<<"triangle is valid";
+        a++;
+    }
+    else
+    {
+        cout<<"triangle is not valid";
+    }
+}
+void triangle::sides()
+{
+    if(a==1)
+    {
+        cout<<"\nsides are: "<<x<<","<<y<<","<<z;
+    }
+}
+void triangle::area()
+{
+    if(a==1)
+    {
+        ar=sqrt(s*(s-x)*(s-y)*(s-z));
+        cout<<"\narea= "<<ar;
+    }
+}
+int main()
+{
+    triangle tri;
+    tri.assign();
+    tri.valid();
+    tri.sides();
+    tri.area();
+}
